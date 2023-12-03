dc75aa784d09784172a949f0f3730b46b352a049
isaac finkelstein <isaacfink123@gmail.com>
2023-12-03 00:20:12 +0000

    added calendar day links

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://aluf.dev',
   markdown: {
     shikiConfig: {
       theme: 'one-dark-pro',
