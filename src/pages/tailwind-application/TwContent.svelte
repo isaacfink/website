ac0c4a23031469330a9ca2db712e105dec950283
isaac finkelstein <isaacfink123@gmail.com>
2024-03-27 16:02:03 +0000

    feat: added read time

--- src/pages/tailwind-application/TwContent.svelte
+++ src/pages/tailwind-application/TwContent.svelte
@@ -44,6 +44,24 @@
   <div
     class="mt-6 space-y-7 text-base text-main-400 dark:text-main-300 col-span-3 scroll-smooth prose prose-a:text-slate-200 prose-a:font-semibold hover:prose-a:underline prose-a:no-underline prose-p:tracking-wide prose-invert"
   >
+    <div class="flex items-center gap-x-2 text-main-500 justify-end">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="w-5 h-5"
+        ><line x1="10" x2="14" y1="2" y2="2" /><line
+          x1="12"
+          x2="15"
+          y1="14"
+          y2="11"
+        /><circle cx="12" cy="14" r="8" /></svg
+      ><span class="text-lg font-ibm">9 minutes</span>
+    </div>
     <h2
       use:intersectionobserver={"who-am-i"}
       class="text-main-200 text-lg font-ibm scroll-mt-6"
