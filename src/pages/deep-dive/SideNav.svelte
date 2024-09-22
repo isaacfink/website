2fbc869bf578643184bddf63b83eecf6458755b2
isaac finkelstein <isaacfink123@gmail.com>
2024-09-22 17:33:05 +0100

    added deep-dive pagge

--- src/pages/deep-dive/SideNav.svelte
+++ src/pages/deep-dive/SideNav.svelte
@@ -9,6 +9,5 @@
 <Link {activeStep} id="my-abilities" name="My skills"></Link>
 <Link {activeStep} id="team-experience" name="Team experience"></Link>
 <Link {activeStep} id="freelance-experience" name="Freelance experience"></Link>
-<Link {activeStep} id="why-tailwind-labs" name="Why Tailwind Labs"></Link>
 <Link {activeStep} id="why-me" name="Why me"></Link>
 <Link {activeStep} id="the-future" name="The future"></Link>
